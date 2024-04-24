c5e6c0b19c68ca6f3a930ba34b6f1c0c7590622f
Chinta Yanggest Br Tarigan <chintacapric836@gmail.com>
2024-04-24 18:08:12 +0700

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
       <div class="label">
       </div>
       {{-- button add --}}
-      <button class="btn btn-success w-36 self-center" @if ($user != 'login') disabled @endif>Add</button>
+      <button class="btn btn-success w-36 self-center" >Add</button>
       {{-- akhir button add --}}
     </label>
     {{-- akhir search bar --}}
@@ -20,14 +20,13 @@
       {{-- task --}}
 	  <div class="flex flex-col gap-3">
     {{-- task 1 --}}
-    @if ($task == 'task1')
     <div role="alert" class="alert max-w-4xl mx-auto">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
       </svg>
       <div class="flex flex-col">
         <span class="text-sm text-slate-400">22-03-2022</span>
-        <span class="text-xl font-bold">{{ $task1 }}</span>
+        <span class="text-xl font-bold"> Task1 </span>
       </div>
       <div>
         <div class="tooltip" data-tip="Detail">
@@ -42,7 +41,6 @@
       </div>
     </div>
     {{-- akhir task 1 --}}
-    @else
     {{-- task 2 --}}
     <div role="alert" class="alert max-w-4xl mx-auto">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -50,7 +48,7 @@
       </svg>
       <div class="flex flex-col">
         <span class="text-sm text-slate-400">22-03-2022</span>
-        <span class="text-xl font-bold">{{ $task2 }}</span>
+        <span class="text-xl font-bold"> Task2 </span>
       </div>
       <div>
         <div class="tooltip" data-tip="Detail">
@@ -65,7 +63,6 @@
       </div>
     </div>
     {{-- akhir task 2 --}}
-    @endif
       </div>
       {{-- akhir task --}}
     </div>
